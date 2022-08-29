69dded0039d6432acb8ce56b19c9bd4cc8c10471
Aris Ripandi <riipandi@gmail.com>
2022-08-29 09:41:17 +0700

    fix vite react dependency

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,1 +1,13 @@
-export default () => console.log('hello world')
+// TODO: FIX React not defined causing by Vite
+// @ts-nocheck
+import * as React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl text-white">Welcome to Tauri!</h1>
+    </div>
+  )
+}
+
+export default App
